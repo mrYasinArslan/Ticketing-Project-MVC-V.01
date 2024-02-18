138d6b75112b627235ff2480b5eebca9827b8e8f
Yasin ARSLAN <mryasinarslan@hotmail.com>
2024-02-18 21:50:04 +0300

    Services implemented

---
+++ src/main/java/com/cydeo/services/RoleService.java
@@ -0,0 +1,28 @@
+package com.cydeo.services;
+
+import com.cydeo.dto.RoleDto;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+
+import java.util.List;
+
+public interface RoleService {
+
+    //save, findById, Delete,Update, FindAll,   crud operator.
+    //when ever you save you are returning this user DTO and repository will take this user DTO and
+    //map it to entity and it is  gone save in  database
+    //Some times after you save, you need to access that object base on  the certain unique thing either
+    //ID primary key or user name, At that time still we  need the object. Thats why you are returning.
+    //And in the error handling we will use this kind of definiton. Basicly when ever we save dont use void, use return object after save.
+
+    RoleDto save(RoleDto roleDto);
+
+    RoleDto findByID(Long id);
+
+    List<RoleDto> findAll();
+
+    void delete(RoleDto role);
+    void deleteByID(Long id);
+
+
+}
---
+++ src/main/java/com/cydeo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.cydeo.services;
+
+import com.cydeo.dto.RoleDto;
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+
+    UserDTO save(UserDTO user);
+
+    UserDTO findByID(String username);
+    List<UserDTO> findAll();
+    void delete(UserDTO user);
+    void deleteByID(String username);
+
+
+
+
+}
